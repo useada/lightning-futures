423abb807e9ac6d7d40ea032b280b5d45fdbdf37
lvlei <lvlei@inke.cn>
2024-03-26 09:43:34 +0800

    工程修改

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,8 @@
-
 #!/bin/bash
-APP_DIR=$(dirname $(readlink -f "$0"))
-export LD_LIBRARY_PATH=$APP_DIR../api/CTP_V6.6.9_20220920/linux64:$LD_LIBRARY_PATH
 
-example runtime
+APP_DIR=$(dirname $(readlink -f "$0"))
+
+# export LD_LIBRARY_PATH=$APP_DIR../api/CTP_V6.6.9_20220920/linux64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$APP_DIR:$LD_LIBRARY_PATH
+
+./example runtime
